328c6b1a389a1533432f1ad0b9d7bfb213dfc3f3
Fábio Neves de Souza <fabionevesdesouza@gmail.com>
2018-01-20 15:12:18 -0200

    .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $( document ).on( "pagecreate", "#list", function( event ) {
     
 
-    $(':checkbox"').on("click", function () {
+    $(':checkbox').on("click", function () {
         $('#meupedido').attr("href", _href + $(this).next("label").html());
     });
     
