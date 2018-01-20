63fe1603b46dd24dcb07ef1846e846fb6dd97b1c
Fábio Neves de Souza <fabionevesdesouza@gmail.com>
2018-01-20 10:22:28 -0200

    .

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-$( document ).on( "pageinit", "#aboutPage", function( event ) {
+$( document ).on( "pagecreate", "#aboutPage", function( event ) {
     alert( "This page was just enhanced by jQuery Mobile!" );
   });
