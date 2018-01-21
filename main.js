aa54c5869562e21ee7e3a65adcd1ebdf6e11b551
Fábio Neves de Souza <fabionevesdesouza@gmail.com>
2018-01-21 20:16:46 -0200

    sdf

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var pedido = new Array();
 
 $( document ).on( "pagebeforeshow", "#view", function( event ) {  
-console.log(pedido);
+console.log("pagebeforeshow");
 
 } );
+
+$( document ).on( "pagebeforeload", "#view", function( event ) {  
+    console.log("pagebeforeload");
+    
+    } );
 
 $( document ).on( "pagecreate", "#list", function( event ) {
     
@@ -18,7 +23,10 @@ $( document ).on( "pagecreate", "#list", function( event ) {
             $('#meupedido').attr("href", _href + item  + "%0A");
             pedido.push(item);
         } else {
-            pedido.pop(item);
+            var idx = pedido.indexOf(search_term); 
+            if (idx !== -1) {
+                pedido.splice(idx, 1);
+            }
         }
     });
     
