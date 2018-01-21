9b48b7c408b33c63d57efd7cca6ff0ea23dce08f
Fábio Neves de Souza <fabionevesdesouza@gmail.com>
2018-01-21 19:05:56 -0200

    .

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $( document ).on( "pagecreate", "#list", function( event ) {
         var _href = $('#meupedido').attr("href");
         $('#meupedido').attr("href", _href + $(this).text()  + "%0A");
         $('#pedido-list').append("<li>"+$(this).text()+"</li>");
-        
+        $('#addedToRequets').popup();
     });
     
   });
